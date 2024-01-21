e4e1ea6eb4b6ff8cab51572b7a757dee501f7506
Tharun Sreejith <tarunsree@gmail.com>
2024-01-21 17:21:48 +0530

    Form

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -2,7 +2,7 @@
 	/*Form settings*/
 	$subj = "New message from the site 'Portfolio/Resume'"; //letter subject
 	$to = 'tarunsree@gmail.com'; // Enter Your E-mail
-	$from = 'tarunsree@gmail.com'; // Admin e-mail
+	$from = 'tarunsree@outlook.com'; // Admin e-mail
 	$fromName = 'TS@TS'; // Your company name
 	$charset = 'UTF-8';
 ?>
